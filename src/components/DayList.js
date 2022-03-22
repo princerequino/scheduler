c4840fbc0cbdf5953f3bb307e6ac5123ed6c7359
princerequino <requino.prince@gmail.com>
2022-03-22 00:15:57 -0400

    add DayListItem test/scss and build DayList.js

---
+++ src/components/DayList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import DayListItem from 'components/DayListItem';
+
+
+export default function DayList(props) {
+  const schedule = props.days.map(dayObj => {  
+    return (
+      <DayListItem
+        key={dayObj.id}
+        name={dayObj.name}
+        spots={dayObj.spots}
+        selected={dayObj.name === props.day}
+        setDay={props.setDay} />         
+    )
+  })
+  return <ul>{schedule}</ul>
+}
+
