530c4739fbbc82ad15e7781ee01eba465c51066c
princerequino <requino.prince@gmail.com>
2022-03-22 23:04:40 -0400

    build appointment form / add state

--- stories/index.js
+++ stories/index.js
@@ -17,6 +17,7 @@ import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
 import Error from "components/Appointment/Error";
+import Form from "components/Appointment/Form";
 
 
 storiesOf("Button", module)
@@ -169,5 +170,21 @@ storiesOf("Button", module)
           <Error
             message="Could not delete appointment."
             onClose={action("onClose")}
+          />
+        ))
+        .add('Create', () => (
+          <Form
+            interviewers={interviewers}
+            onSave={action('onSave')}
+            onCancel={action('onCancel')}
+          />
+        ))    
+        .add('Edit', () => (
+          <Form 
+            name="Bob"
+            interviewers={interviewers}
+            value={3}
+            onSave={action('onSave')}
+            onCancel={action('onCancel')}
           />
         ));
