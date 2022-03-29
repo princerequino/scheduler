0d1d948abba5ed674aea72480efb0a3f5b3febd3
princerequino <requino.prince@gmail.com>
2022-03-28 20:40:07 -0400

    Refactored for concern design separation

--- src/components/Application.js
+++ src/components/Application.js
@@ -8,70 +8,17 @@ import InterviewerList from "./InterviewerList";
 
 import Appointment from "components/Appointment";
 import { getAppointmentsForDay, getInterview, getInterviewersForDay } from "helpers/selectors";
+import useApplicationData from "hooks/useApplicationData";
 
 
 export default function Application(props) {
 
-  const [state, setState] = useState({
-    day: "Monday",
-    days: [],
-    appointments: {}
-  })
-
-  const setDay = day => setState({...state, day});
-
-
-  useEffect(() => {
-    Promise.all([
-      Promise.resolve(axios.get('http://localhost:8001/api/days')),
-      Promise.resolve(axios.get('http://localhost:8001/api/appointments')),
-      Promise.resolve(axios.get('http://localhost:8001/api/interviewers'))
-    ]).then((all) => {
-      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}));
-    })
-  }, [])
-
-  function bookInterview(id, interview) {
-    console.log("State", state)
-    const appointment = {
-      ...state.appointments[id],
-      interview: { ...interview }
-    };
-
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    };
-
-
-    console.log("appointments", appointments)
-    return axios.put(`http://localhost:8001/api/appointments/${id}`, {interview:interview})
-    .then(res => {
-        setState({...state, appointments})
-        return res
-      })
-
-  }
-
-  async function cancelInterview(id) {
-
-    const appointment = {
-      ...state.appointments[id],
-      interview: null
-    };
-
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    }
-
-    return axios.delete(`http://localhost:8001/api/appointments/${id}`)
-    .then(res => {
-      setState({...state, appointments})
-      return res
-    })
-
-  }
+  const {
+    state,
+    setDay,
+    bookInterview,
+    cancelInterview
+  } = useApplicationData();
 
   const appointmentObjects = getAppointmentsForDay(state, state.day);
   const interviewers = getInterviewersForDay(state, state.day);
@@ -128,13 +75,4 @@ export default function Application(props) {
     </main>
   );
 
-    
-          
-            
-    
-
-          
-    
-    
-  
 }
